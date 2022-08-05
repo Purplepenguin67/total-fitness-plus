00f0f45b80120cc686b0148b553d782debced4ed
qing507543 <qingc@usc.edu>
2022-08-04 21:17:50 -0700

    Qing

---
+++ controllers/api/coursesRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { courses } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const courses = await courses.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newcourses);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const coursesData = await courses.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!coursesData) {
+      res.status(404).json({ message: 'No courses found with this id!' });
+      return;
+    }
+
+    res.status(200).json(coursesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
