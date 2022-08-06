a15d4d49c1edbec169a5548660110588ee33f871
qing507543 <qingc@usc.edu>
2022-08-06 10:13:50 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,24 +1,13 @@
 const User = require('./User');
 const courses = require('./courses');
-const express = require('express');
-const app = express();
 
 User.hasMany(courses, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-
 courses.hasone(User, {
     foreignKey: 'courses_id',
   });
-
-
-  app.get('/', (req, res) => {
-    console.log('Showing on terminal!');
-    res.send('I will be on the browser');
-});
-
-app.listen(3000);
 
 module.exports = { User, courses };
