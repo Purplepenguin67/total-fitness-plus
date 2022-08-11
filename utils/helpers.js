3bb34e278118a93a10c361c723bdab019155fb2e
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-08-11 16:56:42 -0700

    first Heroku commit

--- utils/helpers.js
+++ utils/helpers.js
@@ -11,6 +11,8 @@ module.exports = {
     const randomNum = Math.random();
 
     
+
+    
     if (randomNum > 0.7) {
       return `<span for="img" aria-label="lightbulb">💡</span>`;
     } else if (randomNum > 0.4) {
